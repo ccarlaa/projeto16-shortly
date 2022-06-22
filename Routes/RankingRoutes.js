919928e0f76ace6a55554a6e2ec20e33ecdeb4e1
isabellacgmsa <isabellacgmsa1@gmail.com>
2022-06-22 09:24:16 -0300

    Preparando para revisão de código

--- Routes/RankingRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { rankingGetController } from "../Controllers/RankingControllers.js"
-
-const rankingRoute = Router()
-
-rankingRoute.get('/ranking', rankingGetController)
-
-export default rankingRoute
--- Routes/SignInRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import { signInMiddleware } from "../Middlewares/SignInMiddlewares.js"
-import { signInController } from "../Controllers/SignInControllers.js"
-
-const signInRoute = Router()
-
-signInRoute.post('/signin', signInMiddleware, signInController )
-
-export default signInRoute
